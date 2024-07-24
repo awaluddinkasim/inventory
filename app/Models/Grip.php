d88d5f3fb6e5a80686ef9660ce7d83b051210124
Awaluddin Kasim <awaluddinkasim7@gmail.com>
2024-07-24 16:14:42 +0800

    update models

--- app/Models/Grip.php
+++ app/Models/Grip.php
@@ -2,10 +2,28 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Grip extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'model_id',
+        'size_id',
+        'color',
+        'weight',
+        'core_size',
+        'wholesale',
+        'percent'
+    ];
+
+    public function retail(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => $this->wholesale + ($this->wholesale * $this->percent)
+        );
+    }
 }
--- app/Models/GripModel.php
+++ app/Models/GripModel.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class GripModel extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'nama',
+        'url',
+    ];
 }
