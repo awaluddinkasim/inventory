e78819be8bb1569ae61cd1047c2db5e7728091aa
Awaluddin Kasim <awaluddinkasim7@gmail.com>
2024-07-24 16:09:01 +0800

    update users migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,8 +15,10 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('phone');
+            $table->enum('role', ['admin', 'user'])->default('user');
+            $table->timestamp('last_login')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
