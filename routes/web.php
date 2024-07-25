ff7660b2ce2c2e13a2c2cbd4d0333a9c911afa5e
Awaluddin Kasim <awaluddinkasim7@gmail.com>
2024-07-26 07:21:41 +0800

    update routes

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,17 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('index');
+    return redirect()->route('dashboard');
 });
-Route::get('/login', function () {
-    return view('auth');
+
+Route::middleware('guest')->group(function () {
+    Route::get('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/login', [AuthController::class, 'authenticate'])->name('authenticate');
 });
+
+Route::get('/dashboard', function () {
+    return view('pages.dashboard');
+})->name('dashboard');
