8b4ea08ab0c3c159bd99d76351f26d1a004776c9
Awaluddin Kasim <awaluddinkasim7@gmail.com>
2024-07-26 15:01:57 +0800

    add stock relation

--- app/Models/Grip.php
+++ app/Models/Grip.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Grip extends Model
 {
@@ -24,6 +25,11 @@ class Grip extends Model
     public function model(): BelongsTo
     {
         return $this->belongsTo(GripModel::class, 'model_id');
+    }
+
+    public function stock(): HasOne
+    {
+        return $this->hasOne(Stock::class);
     }
 
     public function retail(): Attribute
