ca598164b4fa9f32b05ffd3d64ea1ecc08f77ac5
Awaluddin Kasim <awaluddinkasim7@gmail.com>
2024-07-26 08:13:12 +0800

    menu config

--- config/menu.php
+++ config/menu.php
@@ -5,6 +5,7 @@ return [
         'label' => 'Dashboard',
         'icon' => 'bx-home-circle',
         'route-name' => 'dashboard',
+        'admin-only' => false,
     ],
     [
         'label' => 'Master Data',
@@ -12,32 +13,37 @@ return [
         'submenu' => [
             [
                 'label' => 'Model',
-                // 'route-name' => '#',
+                'route-name' => 'master.model',
             ],
             [
                 'label' => 'Size',
-                // 'route-name' => '#',
+                'route-name' => 'master.size',
             ],
         ],
+        'admin-only' => false,
     ],
     [
         'label' => 'Data Grip',
         'icon' => 'bx-dock-top',
-        // 'route-name' => '#',
+        'route-name' => 'grips',
+        'admin-only' => false,
     ],
     [
         'label' => 'Stock',
         'icon' => 'bx-collection',
         // 'route-name' => '#',
+        'admin-only' => false,
     ],
     [
         'label' => 'Barcode',
         'icon' => 'bx-barcode',
         // 'route-name' => '#',
+        'admin-only' => false,
     ],
     [
         'label' => 'Users',
         'icon' => 'bx-user',
-        // 'route-name' => '#',
+        'route-name' => 'users',
+        'admin-only' => true,
     ],
 ];
