12dcb1382c6f3080a68043c6ee79dc01f9b99a69
Awaluddin Kasim <awaluddinkasim7@gmail.com>
2024-07-28 04:54:23 +0800

    update livewire components

--- resources/views/livewire/grip-model.blade.php
+++ resources/views/livewire/grip-model.blade.php
@@ -24,7 +24,8 @@
                     <td><a href="{{ $model->url }}" target="_blank">{{ $model->url }}</a></td>
 
                     <td class="text-center">
-                        <button type="button" class="btn btn-primary" wire:click="edit({{ $model->id }})">
+                        <button type="button" class="btn btn-primary" wire:click="edit({{ $model->id }})"
+                            wire:loading.attr="disabled">
                             Edit
                         </button>
                         <form action="{{ route('master.model.destroy', $model->id) }}" class="d-inline" method="POST">
@@ -39,38 +40,27 @@
         </tbody>
     </x-component.table>
 
-    <div class="modal fade" id="editModal" tabindex="-1" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel1">Edit Data</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <form
-                    action="@if ($currentEdit) {{ route('master.model.update', $currentEdit->id) }} @endif"
-                    method="POST" autocomplete="off">
-                    @csrf
-                    @method('PATCH')
-                    <div class="modal-body">
-                        <x-form.select label="Type" name="type_id" id="typeSelect" :required="true">
-                            @foreach ($types as $type)
-                                <option value="{{ $type->id }}" @if ($currentEdit?->type_id == $type->id) selected @endif>
-                                    {{ $type->mfg }} - {{ $type->name }}</option>
-                            @endforeach
-                        </x-form.select>
-                        <x-form.input label="Name" name="name" id="nameInput" :value="$currentEdit?->name"
-                            :required="true" />
-                        <x-form.input label="URL" name="url" id="urlInput" :value="$currentEdit?->url"
-                            :required="true" />
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">
-                            Close
-                        </button>
-                        <button type="submit" class="btn btn-primary">Save Changes</button>
-                    </div>
-                </form>
+    <x-component.modal id="editModal" title="Edit Data">
+        <form action="@if ($currentEdit) {{ route('master.model.update', $currentEdit->id) }} @endif"
+            method="POST" autocomplete="off">
+            @csrf
+            @method('PATCH')
+            <div class="modal-body">
+                <x-form.select label="Type" name="type_id" id="typeSelect" :required="true">
+                    @foreach ($types as $type)
+                        <option value="{{ $type->id }}" @if ($currentEdit?->type_id == $type->id) selected @endif>
+                            {{ $type->mfg }} - {{ $type->name }}</option>
+                    @endforeach
+                </x-form.select>
+                <x-form.input label="Name" name="name" id="nameInput" :value="$currentEdit?->name" :required="true" />
+                <x-form.input label="URL" name="url" id="urlInput" :value="$currentEdit?->url" :required="true" />
             </div>
-        </div>
-    </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">
+                    Close
+                </button>
+                <button type="submit" class="btn btn-primary">Save Changes</button>
+            </div>
+        </form>
+    </x-component.modal>
 </div>
--- resources/views/livewire/grip-type.blade.php
+++ resources/views/livewire/grip-type.blade.php
@@ -22,7 +22,8 @@
                     <td>{{ $type->name }}</td>
 
                     <td class="text-center">
-                        <button type="button" class="btn btn-primary" wire:click="edit({{ $type->id }})">
+                        <button type="button" class="btn btn-primary" wire:click="edit({{ $type->id }})"
+                            wire:loading.attr="disabled">
                             Edit
                         </button>
                         <form action="{{ route('master.type.destroy', $type->id) }}" class="d-inline" method="POST">
@@ -36,33 +37,21 @@
         </tbody>
     </x-component.table>
 
-    <div class="modal fade" id="editModal" tabindex="-1" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel1">Edit Data</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <form
-                    action="@if ($currentEdit) {{ route('master.type.update', $currentEdit->id) }} @endif"
-                    method="POST" autocomplete="off">
-                    @csrf
-                    @method('PATCH')
-                    <div class="modal-body">
-                        <x-form.input label="MFG" name="mfg" id="mfgInput" :value="$currentEdit?->mfg"
-                            :required="true" />
-                        <x-form.input label="Name" name="name" id="nameInput" :value="$currentEdit?->name"
-                            :required="true" />
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">
-                            Close
-                        </button>
-                        <button type="submit" class="btn btn-primary">Save Changes</button>
-                    </div>
-                </form>
+    <x-component.modal id="editModal" title="Edit Data">
+        <form action="@if ($currentEdit) {{ route('master.type.update', $currentEdit->id) }} @endif"
+            method="POST" autocomplete="off">
+            @csrf
+            @method('PATCH')
+            <div class="modal-body">
+                <x-form.input label="MFG" name="mfg" id="mfgInput" :value="$currentEdit?->mfg" :required="true" />
+                <x-form.input label="Name" name="name" id="nameInput" :value="$currentEdit?->name" :required="true" />
             </div>
-        </div>
-    </div>
-
+            <div class="modal-footer">
+                <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">
+                    Close
+                </button>
+                <button type="submit" class="btn btn-primary">Save Changes</button>
+            </div>
+        </form>
+    </x-component.modal>
 </div>
