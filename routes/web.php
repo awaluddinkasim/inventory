e4021684356ee445dc074ee01c8c4a3ad25b2e13
Awaluddin Kasim <awaluddinkasim7@gmail.com>
2024-07-28 05:18:37 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -38,8 +38,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/grips', [GripController::class, 'index'])->name('grips');
     Route::post('/grips', [GripController::class, 'store'])->name('grips.store');
     Route::get('/grips/{grip}', [GripController::class, 'show'])->name('grips.show');
-    Route::get('/grips/{grip}/edit', [GripController::class, 'edit'])->name('grips.edit')->can('update', 'grip');
-    Route::patch('/grips/{grip}', [GripController::class, 'update'])->name('grips.update')->can('update', 'grip');
+    Route::get('/grips/{grip}/edit', [GripController::class, 'edit'])->name('grips.edit');
+    Route::patch('/grips/{grip}', [GripController::class, 'update'])->name('grips.update');
     Route::delete('/grips/{grip}', [GripController::class, 'destroy'])->name('grips.destroy')->can('delete', 'grip');
 
     Route::get('/stock', [StockController::class, 'index'])->name('stock');
