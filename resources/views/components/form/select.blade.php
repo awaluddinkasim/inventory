b12d0da9997ca82bf5f2e699651d5b25193e9264
Awaluddin Kasim <awaluddinkasim7@gmail.com>
2024-07-28 07:58:12 +0800

    update

--- resources/views/components/form/select.blade.php
+++ resources/views/components/form/select.blade.php
@@ -4,7 +4,7 @@
     <label for="{{ $id }}" class="form-label">{{ $label }}</label>
     <select class="form-select @error($name) is-invalid @enderror" id="{{ $id }}" name="{{ $name }}"
         @if ($required) required @endif aria-label="{{ $label }}">
-        <option value="" selected hidden>Pilih</option>
+        <option value="" selected hidden>Choose</option>
         {{ $slot }}
     </select>
 </div>
