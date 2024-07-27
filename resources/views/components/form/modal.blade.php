13bd99f5b3e4342cc468b5c8cf8a9a0ead5b11a5
Awaluddin Kasim <awaluddinkasim7@gmail.com>
2024-07-28 04:54:02 +0800

    update modal component

--- resources/views/components/form/modal.blade.php
+++ resources/views/components/form/modal.blade.php
@@ -5,26 +5,17 @@
 </button>
 
 <!-- Modal -->
-<div class="modal fade" id="formModal" tabindex="-1" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel1">{{ $title }}</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <form action="{{ $action }}" method="POST" autocomplete="off">
-                @csrf
-                <div class="modal-body">
-                    {{ $slot }}
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">
-                        Close
-                    </button>
-                    <button type="submit" class="btn btn-primary">Submit</button>
-                </div>
-            </form>
-
+<x-component.modal id="formModal" :title="$title">
+    <form action="{{ $action }}" method="POST" autocomplete="off">
+        @csrf
+        <div class="modal-body">
+            {{ $slot }}
         </div>
-    </div>
-</div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">
+                Close
+            </button>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </div>
+    </form>
+</x-component.modal>
