f62b1295c7065d969da4023c1514c685cbb12cb2
Awaluddin Kasim <awaluddinkasim7@gmail.com>
2024-07-28 05:05:47 +0800

    update stock

--- app/Models/Stock.php
+++ app/Models/Stock.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -20,6 +21,13 @@ class Stock extends Model
     public function grip(): BelongsTo
     {
         return $this->belongsTo(Grip::class);
+    }
+
+    public function date(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => Carbon::parse($value)->isoFormat('DD MMMM YYYY')
+        );
     }
 
     public function amount(): Attribute
--- resources/views/pages/stock/index.blade.php
+++ resources/views/pages/stock/index.blade.php
@@ -2,14 +2,15 @@
     <div class="card">
         <div class="card-header">
             <div class="d-flex justify-content-between align-items-center">
-                <h5 class="card-title">Models</h5>
-                <x-form.modal label="Stock" title="Form Model" action="{{ route('stock.store') }}">
-                    <x-form.select label="Grip" name="grip_id" id="gripSelect" :required="true">
+                <h5 class="card-title">Stock</h5>
+                <x-form.modal label="New Stock" title="Form Model" action="{{ route('stock.store') }}">
+                    <x-form.select label="Grip Model" name="grip_id" id="gripSelect" :required="true">
                         @foreach ($grips as $grip)
-                            <option value="{{ $grip->id }}">{{ $grip->model->name }} </option>
+                            <option value="{{ $grip->id }}">{{ $grip->model->name }} - {{ $grip->size }} </option>
                         @endforeach
                     </x-form.select>
-                    <x-form.input label="Quantity" name="quantity" type="number" id="quantityInput" :required="true" />
+                    <x-form.input label="Quantity" name="quantity" type="number" id="quantityInput"
+                        :required="true" />
                     <x-form.input label="Date" name="date" type="date" id="dateInput" :required="true" />
                 </x-form.modal>
             </div>
@@ -18,21 +19,28 @@
             <x-component.datatable id="stockTable">
                 <thead>
                     <th>#</th>
-                    <th>Grip Model Name</th>
+                    <th>Grip Model</th>
+                    <th>Size</th>
+                    <th>Wholesale Price</th>
                     <th>Quantity</th>
                     <th>Amount</th>
-                    <th>Date</th>
+                    <th>Stock Date</th>
                     <th></th>
                 </thead>
                 <tbody>
                     @foreach ($stocks as $stock)
                         <tr>
                             <td>{{ $loop->iteration }}</td>
-                            <td>{{ $stock->grip->model->name }}</td>
+                            <td>
+                                <a href="{{ $stock->grip->model->url }}" target="_blank">
+                                    {{ $stock->grip->model->name }}
+                                </a>
+                            </td>
+                            <td>{{ $stock->grip->size }}</td>
+                            <td>Rp. {{ number_format($stock->grip->wholesale) }}</td>
                             <td>{{ $stock->quantity }}</td>
-                            <td>{{ $stock->amount }}</td>
+                            <td>Rp. {{ number_format($stock->amount) }}</td>
                             <td>{{ $stock->date }}</td>
-                            {{-- <td><a href="{{ $stock->url }}" target="_blank">{{ $stock->url }}</a></td> --}}
 
                             <td class="text-center">
                                 <form action="{{ route('stock.destroy', $stock->id) }}" class="d-inline"
@@ -42,12 +50,10 @@
                                     <x-component.button type="submit" label="Delete" color="danger" />
                                 </form>
                             </td>
-
                         </tr>
                     @endforeach
                 </tbody>
             </x-component.datatable>
         </div>
     </div>
-
 </x-layout>
