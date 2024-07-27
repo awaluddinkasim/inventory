74ab2cedabdad60a6a7fe1aa00b44a9651e0e240
Awaluddin Kasim <awaluddinkasim7@gmail.com>
2024-07-27 12:41:38 +0800

    add new form component

---
+++ resources/views/components/form/datalist.blade.php
@@ -0,0 +1,11 @@
+@props(['label', 'id', 'name', 'required' => false, 'value' => null])
+
+<div class="mb-3">
+    <label class="form-label" for="{{ $id }}">{{ $label }}</label>
+    <input name="{{ $name }}" value="{{ $value }}" list="{{ $name }}Options"
+        class="form-control @error($name) is-invalid @enderror" id="{{ $id }}"
+        @if ($required) required @endif>
+    <datalist id="{{ $name }}Options">
+        {{ $slot }}
+    </datalist>
+</div>
