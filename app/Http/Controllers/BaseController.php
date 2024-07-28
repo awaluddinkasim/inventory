952c475cac69c9c1df2a886b0e5e37333fd5c461
Awaluddin Kasim <awaluddinkasim7@gmail.com>
2024-07-28 08:15:57 +0800

    update base controller

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -3,12 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
 use InvalidArgumentException;
 
 class BaseController extends Controller
 {
-    public function redirect(string $redirectTo, array $response): RedirectResponse
+    protected function redirect(string $redirectTo, array $response): RedirectResponse
     {
         if (!isset($response['status']) || !isset($response['message'])) {
             throw new InvalidArgumentException("Array response harus memiliki key 'status' dan 'message'");
@@ -17,7 +16,7 @@ class BaseController extends Controller
         return redirect($redirectTo)->with($response['status'], $response['message']);
     }
 
-    public function redirectBack(array $response, bool $withInput = false): RedirectResponse
+    protected function redirectBack(array $response, bool $withInput = false): RedirectResponse
     {
         if (!isset($response['status']) || !isset($response['message'])) {
             throw new InvalidArgumentException("Array response harus memiliki key 'status' dan 'message'");
