a66328ff47ecf83b132d2df68bb845dfb9319f32
Awaluddin Kasim <awaluddinkasim7@gmail.com>
2024-07-28 11:06:56 +0800

    update code generator

--- app/helpers.php
+++ app/helpers.php
@@ -12,7 +12,13 @@ if (!function_exists('convertToNumber')) {
 if (!function_exists('generateCode')) {
     function generateCode($color, $model, $size): string
     {
-        $colorCode = substr($color, 0, 2);
+        $color = trim($color);
+        if (str_contains($color, '/')) {
+            $words = explode('/', $color);
+            $colorCode = strtolower($words[0][0] . $words[1][0]);
+        } else {
+            $colorCode = substr($color, 0, 2);
+        }
         $modelId = str_pad($model, 6, '0', STR_PAD_LEFT);
         $sizeCode = substr($size, 0, 1);
 
