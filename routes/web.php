bc81e14b7a8bf173aa9d3f28da1359cb97d66863
Awaluddin Kasim <awaluddinkasim7@gmail.com>
2024-08-09 21:34:03 +0800

    middleware fix

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ Route::get('/', function () {
     return redirect()->route('dashboard');
 });
 
-Route::middleware('guest')->group(function () {
+Route::middleware(['guest:user,admin'])->group(function () {
     Route::get('/login', [AuthController::class, 'login'])->name('login');
     Route::post('/login', [AuthController::class, 'authenticate'])->name('authenticate');
 });
