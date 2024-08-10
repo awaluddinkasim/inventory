5a671e70333be2d3e6b351110ef841d17a66c6e5
MrKrab28 <mr.krab2810@gmail.com>
2024-08-10 15:35:04 +0800

    -fix

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
-use Illuminate\Http\RedirectResponse;
+use Illuminate\View\View;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\View\View;
+use Illuminate\Http\RedirectResponse;
+use App\Http\Controllers\BaseController;
 
 class UserController extends BaseController
 {
