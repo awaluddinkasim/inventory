bd647b34fd0bd5e10ab2e42be68b5ce10a73ea6a
Awaluddin Kasim <awaluddinkasim7@gmail.com>
2024-08-10 15:12:56 +0800

    update middleware

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -15,7 +15,7 @@ class AdminMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if (!auth()->check() || auth()->user()->role != 'admin') {
+        if (!auth('admin')->check() || auth('admin')->user()->role != 'admin') {
             abort(403, 'Unauthorized action.');
         }
 
