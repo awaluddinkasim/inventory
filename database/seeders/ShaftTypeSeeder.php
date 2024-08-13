de35ba51a33a1987e72499d6ce0d04134903cec1
Awaluddin Kasim <awaluddinkasim7@gmail.com>
2024-08-13 12:11:11 +0800

    shaft type seeder

---
+++ database/seeders/ShaftTypeSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\ShaftType;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ShaftTypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $types = [
+            ['brand' => 'Graphite Design', 'name' => 'Tour AD VF'],
+            ['brand' => 'Graphite Design', 'name' => 'Tour AD CQ'],
+            ['brand' => 'Graphite Design', 'name' => 'Tour AD UB'],
+        ];
+
+        ShaftType::insert($types);
+    }
+}
