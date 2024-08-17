d0ebaf3500b0679a4a411c4410400f7c93353abd
Awaluddin Kasim <awaluddinkasim7@gmail.com>
2024-08-17 17:25:54 +0800

    reports

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -2,13 +2,98 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\GripModel;
+use App\Models\GripPurchase;
+use App\Models\GripSale;
+use App\Models\Shaft;
+use App\Models\ShaftPurchase;
+use App\Models\ShaftSale;
+use Carbon\Carbon;
 use Illuminate\View\View;
 
 class ReportController extends Controller
 {
     public function index(): View
     {
-        return view('pages.reports');
+        $currentMonth = Carbon::now()->startOfYear();
+        $endOfMonth = Carbon::now()->endOfYear();
+
+        $gripSales = [];
+        $shaftSales = [];
+
+        $gripPurchases = [];
+        $shaftPurchases = [];
+
+        while ($currentMonth <= $endOfMonth) {
+            $gripSales[] = GripSale::whereYear('date', $currentMonth->year)
+                ->whereMonth('date', $currentMonth->month)
+                ->get()->sum('amount');
+
+            $shaftSales[] = ShaftSale::whereYear('date', $currentMonth->year)
+                ->whereMonth('date', $currentMonth->month)
+                ->get()->sum('amount');
+
+            $gripPurchases[] = GripPurchase::whereYear('date', $currentMonth->year)
+                ->whereMonth('date', $currentMonth->month)
+                ->get()->sum('amount');
+
+            $shaftPurchases[] = ShaftPurchase::whereYear('date', $currentMonth->year)
+                ->whereMonth('date', $currentMonth->month)
+                ->get()->sum('amount');
+
+            $currentMonth = $currentMonth->addMonth();
+        }
+
+        $cumulativeGripSales = [];
+        $cumulativeShaftSales = [];
+        $currentMonth = Carbon::now()->startOfYear();
+
+        $lastCumulativeGripSales = 0;
+        $lastCumulativeShaftSales = 0;
+        while ($currentMonth <= Carbon::now()) {
+            $index = $currentMonth->isoFormat('MMM');
+            $cumulativeGripSales[$index] = 0;
+            $cumulativeShaftSales[$index] = 0;
+
+            $gs = GripSale::whereYear('date', $currentMonth->year)
+                ->whereMonth('date', $currentMonth->month)
+                ->get()->sum('amount');
+
+            $ss = ShaftSale::whereYear('date', $currentMonth->year)
+                ->whereMonth('date', $currentMonth->month)
+                ->get()->sum('amount');
+
+            $cumulativeGripSales[$index] += $gs + $lastCumulativeGripSales;
+            $cumulativeShaftSales[$index] += $ss + $lastCumulativeShaftSales;
+
+            $lastCumulativeGripSales += $gs;
+            $lastCumulativeShaftSales += $ss;
+
+            $currentMonth = $currentMonth->addMonth();
+        }
+
+        $gripStocks = [];
+        $shaftStocks = [];
+
+        GripModel::all()->take(7)->each(function ($model) use (&$gripStocks) {
+            $gripStocks[$model->name] = $model->grips->sum('stock');
+        });
+        Shaft::all()->take(7)->each(function ($shaft) use (&$shaftStocks) {
+            $shaftStocks[$shaft->shaft] = $shaft->stock;
+        });
+
+        asort($gripStocks);
+        asort($shaftStocks);
+
+        return view('pages.reports', [
+            'gripSales' => $gripSales,
+            'shaftSales' => $shaftSales,
+            'gripPurchases' => $gripPurchases,
+            'shaftPurchases' => $shaftPurchases,
+            'cumulativeGripSales' => $cumulativeGripSales,
+            'cumulativeShaftSales' => $cumulativeShaftSales,
+            'gripStocks' => $gripStocks,
+            'shaftStocks' => $shaftStocks,
+        ]);
     }
 }
--- resources/views/pages/reports.blade.php
+++ resources/views/pages/reports.blade.php
@@ -1,3 +1,266 @@
-<x-layout title="Reports">
+@push('styles')
+    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/apex-charts/apex-charts.css') }}">
+@endpush
 
+@push('scripts')
+    <script src="{{ asset('assets/vendor/libs/apex-charts/apexcharts.js') }}"></script>
+    <script>
+        var monthlySalesOptions = {
+            series: [{
+                name: 'Grip',
+                data: @json($gripSales)
+            }, {
+                name: 'Shaft',
+                data: @json($shaftSales)
+            }],
+            chart: {
+                type: 'bar',
+                height: 300,
+                toolbar: {
+                    show: false
+                }
+            },
+            plotOptions: {
+                bar: {
+                    horizontal: false,
+                    columnWidth: '55%',
+                    endingShape: 'rounded'
+                },
+            },
+            dataLabels: {
+                enabled: false
+            },
+            stroke: {
+                show: true,
+                width: 2,
+                colors: ['transparent']
+            },
+            xaxis: {
+                categories: [
+                    'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+                ],
+            },
+            yaxis: {
+                title: {
+                    text: 'Sales'
+                },
+                labels: {
+                    formatter: (val) => {
+                        return "Rp. " + val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+                    },
+                }
+            },
+            fill: {
+                opacity: 1
+            },
+            tooltip: {
+                y: {
+                    formatter: function(val) {
+                        return "Rp. " + val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+                    }
+                }
+            },
+        };
+        var chart = new ApexCharts(document.querySelector("#monthlySalesChart"), monthlySalesOptions);
+        chart.render();
+
+        var monthlyPurchasesOptions = {
+            series: [{
+                name: 'Grip',
+                data: @json($gripPurchases)
+            }, {
+                name: 'Shaft',
+                data: @json($shaftPurchases)
+            }],
+            chart: {
+                type: 'bar',
+                height: 300,
+                toolbar: {
+                    show: false
+                }
+            },
+            plotOptions: {
+                bar: {
+                    horizontal: false,
+                    columnWidth: '55%',
+                    endingShape: 'rounded'
+                },
+            },
+            dataLabels: {
+                enabled: false
+            },
+            stroke: {
+                show: true,
+                width: 2,
+                colors: ['transparent']
+            },
+            xaxis: {
+                categories: [
+                    'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+                ],
+            },
+            yaxis: {
+                title: {
+                    text: 'Purchases'
+                },
+                labels: {
+                    formatter: (val) => {
+                        return "Rp. " + val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+                    },
+                }
+            },
+            fill: {
+                opacity: 1
+            },
+            tooltip: {
+                y: {
+                    formatter: function(val) {
+                        return "Rp. " + val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+                    }
+                }
+            },
+        };
+        var chart = new ApexCharts(document.querySelector("#monthlyPurchasesChart"), monthlyPurchasesOptions);
+        chart.render();
+
+        var cumulativeSalesOptions = {
+            series: [{
+                    name: 'Grip',
+                    data: @json(array_values($cumulativeGripSales))
+                },
+                {
+                    name: 'Shaft',
+                    data: @json(array_values($cumulativeShaftSales))
+                }
+            ],
+            chart: {
+                height: 300,
+                type: 'area',
+                toolbar: {
+                    show: false
+                }
+            },
+            dataLabels: {
+                enabled: false
+            },
+            stroke: {
+                curve: 'smooth'
+            },
+            xaxis: {
+                categories: @json(array_keys($cumulativeGripSales))
+            },
+            yaxis: {
+                labels: {
+                    formatter: (val) => {
+                        return "Rp. " + val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+                    },
+                }
+            },
+            tooltip: {
+                y: {
+                    formatter: function(val) {
+                        return "Rp. " + val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+                    }
+                }
+            },
+        };
+
+        var cumulativeSales = new ApexCharts(document.querySelector("#cumulativeSales"), cumulativeSalesOptions);
+        cumulativeSales.render();
+
+        var gripStocksOptions = {
+            series: [{
+                name: 'Stock',
+                data: @json(array_values($gripStocks))
+            }],
+            chart: {
+                type: 'bar',
+                height: 350,
+                toolbar: {
+                    show: false
+                }
+            },
+            plotOptions: {
+                bar: {
+                    borderRadius: 4,
+                    borderRadiusApplication: 'end',
+                    horizontal: true,
+                }
+            },
+            dataLabels: {
+                enabled: false
+            },
+            xaxis: {
+                categories: @json(array_keys($gripStocks)),
+            }
+        };
+
+        var gripStocksChart = new ApexCharts(document.querySelector("#gripStocksChart"), gripStocksOptions);
+        gripStocksChart.render();
+
+        var shaftStocksOptions = {
+            series: [{
+                name: 'Stock',
+                data: @json(array_values($shaftStocks))
+            }],
+            chart: {
+                type: 'bar',
+                height: 350,
+                toolbar: {
+                    show: false
+                }
+            },
+            plotOptions: {
+                bar: {
+                    borderRadius: 4,
+                    borderRadiusApplication: 'end',
+                    horizontal: true,
+                }
+            },
+            dataLabels: {
+                enabled: false
+            },
+            xaxis: {
+                categories: @json(array_keys($shaftStocks)),
+            }
+        };
+
+        var shaftStocksChart = new ApexCharts(document.querySelector("#shaftStocksChart"), shaftStocksOptions);
+        shaftStocksChart.render();
+    </script>
+@endpush
+
+<x-layout title="Reports">
+    <div class="row">
+        <div class="col-lg-7">
+            <div class="card my-2">
+                <h5 class="card-header m-0 pb-3">Monthly Sales</h5>
+                <div id="monthlySalesChart" class="px-2"></div>
+            </div>
+        </div>
+        <div class="col-lg-5">
+            <div class="card my-2">
+                <h5 class="card-header m-0 pb-3">Cumulative Sales</h5>
+                <div id="cumulativeSales" class="px-2"></div>
+            </div>
+        </div>
+    </div>
+    <div class="card my-2">
+        <h5 class="card-header m-0 pb-3">Monthly Purchases</h5>
+        <div id="monthlyPurchasesChart" class="px-2"></div>
+    </div>
+    <div class="row">
+        <div class="col-lg-6">
+            <div class="card my-2">
+                <h5 class="card-header m-0 pb-3">Lowest Grip Stocks</h5>
+                <div id="gripStocksChart" class="px-2"></div>
+            </div>
+        </div>
+        <div class="col-lg-6">
+            <div class="card my-2">
+                <h5 class="card-header m-0 pb-3">Lowest Shaft Stocks</h5>
+                <div id="shaftStocksChart" class="px-2"></div>
+            </div>
+        </div>
+    </div>
 </x-layout>
